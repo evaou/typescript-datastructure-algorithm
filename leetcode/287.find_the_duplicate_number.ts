1de9c9a09e748b978e51def0060e9762e98a268d
evaou <speeder528@gmail.com>
2020-12-26 22:16:19 +0800

    solve leetcode 287

---
+++ leetcode/287.find_the_duplicate_number.ts
@@ -0,0 +1,27 @@
+function findDuplicate(nums: number[]): number {
+    let map = new Map<number, number>();
+    let key: number;
+
+    for (let i = 0; i < nums.length; i++) {
+        key = nums[i];
+        if (!map.get(key)) {
+            map.set(key, 1);
+        } else {
+            return key;
+        }
+    }
+
+    return undefined;
+};
+
+var arr = [1,3,4,2,2];
+console.log(findDuplicate(arr)); // 2
+
+arr = [2, 5, 1, 2, 3, 5, 1, 2, 4];
+console.log(findDuplicate(arr)); // 2
+
+arr = [2, 1, 1, 2, 3, 5, 1, 2, 4]; 
+console.log(findDuplicate(arr)); // 1 
+
+arr = [2, 3, 4, 5]; 
+console.log(findDuplicate(arr)); // undefined
