6acec7fc7cf25f3ed1468a604e1903bd2f0110fb
evaou <speeder528@gmail.com>
2020-12-27 22:17:33 +0800

    add stack by array

---
+++ data-structure/stack-by-array.ts
@@ -0,0 +1,44 @@
+class Stack {
+  array: string[];
+
+  constructor() {
+    this.array = [];
+  }
+
+  peek(): string | null {
+    return this.array[this.array.length - 1];
+  }
+
+  pop(): string | null {
+    if (this.array.length === 0) {
+      return null;
+    }
+
+    return this.array.pop();
+  }
+
+  push(value: string): Stack {
+    this.array.push(value);
+
+    return this;
+  }
+
+  isEmpty(): boolean {
+    if (this.array.length === 0) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
+
+const myStack = new Stack();
+myStack.push("google");
+myStack.push("udemy");
+myStack.push("discord");
+console.log(myStack);
+console.log(myStack.peek());
+
+console.log(myStack.pop());
+console.log(myStack.pop());
+console.log(myStack.pop());
