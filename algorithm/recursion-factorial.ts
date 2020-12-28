872fb75d4c5edc019b357b425b6b4984d4f8eedd
evaou <speeder528@gmail.com>
2020-12-28 22:32:16 +0800

    add recursive and iterative factorial

---
+++ algorithm/recursion-factorial.ts
@@ -0,0 +1,27 @@
+function findFactorialRecursive(number: number) {
+    if (number <= 2) {
+        return number;
+    }
+
+    let answer: number;
+    answer = number * findFactorialRecursive(number - 1);
+
+    return answer;
+}
+
+function findFactorialIterative(number: number) {
+    let answer: number = 1;
+
+    for (let i = 2; i <= number; i++) {
+        answer *= i;
+    }
+
+    return answer;
+}
+
+var result: number;
+result = findFactorialRecursive(5);
+console.log(result);
+
+result = findFactorialIterative(5);
+console.log(result);
