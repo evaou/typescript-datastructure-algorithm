235dd1458fb10b94e877a00caaf78600b2ab1033
evaou <speeder528@gmail.com>
2020-12-29 06:57:22 +0800

    add recursive and iterative fibonacci

---
+++ algorithm/recursion-fibonacci.ts
@@ -0,0 +1,28 @@
+function fibonacciIterative(num: number): number {
+    if (num <= 1) {
+        return num;
+    }
+
+    let sum: number = 1;
+    let first: number = 0;
+    let second: number = 1;
+
+    for (let i = 2; i <= num; i++) {
+        sum = first + second;
+        first = second;
+        second = sum;
+    }
+
+    return sum;
+}
+
+function fibonacciRecursive(num): number {
+    if (num <= 1) {
+        return num;
+    }
+
+    return fibonacciRecursive(num - 1) + fibonacciRecursive(num - 2);
+}
+
+console.log(fibonacciIterative(6));
+console.log(fibonacciRecursive(6));
