88c14562d815ef49d3d408d987c25f4eadbdde8b
evaou <speeder528@gmail.com>
2020-12-28 13:28:55 +0800

    add namespace and update format

--- data-structure/stack-by-linked-list.ts
+++ data-structure/stack-by-linked-list.ts
@@ -1,79 +1,81 @@
-class MyNode {
-  value: string;
-  next: MyNode | null;
+namespace StackByLinkedList {
+    class Node {
+        value: string;
+        next: Node | null;
 
-  constructor(value: string) {
-    this.value = value;
-    this.next = null;
-  }
+        constructor(value: string) {
+            this.value = value;
+            this.next = null;
+        }
+    }
+
+    class Stack {
+        top: Node | null;
+        bottom: Node | null;
+        length: number;
+
+        constructor() {
+            this.top = null;
+            this.bottom = null;
+            this.length = 0;
+        }
+
+        peek(): Node | null {
+            return this.top;
+        }
+
+        pop(): Node | null {
+            if (!this.top) {
+                return null;
+            }
+
+            let result: Node | null = null;
+
+            result = this.top;
+            this.top = this.top.next;
+            result.next = null;
+            this.length--;
+
+            if (this.length === 0) {
+                this.bottom = null;
+            }
+
+            return result;
+        }
+
+        push(value: string): Stack {
+            const newNode: Node = new Node(value);
+
+            if (this.length === 0) {
+                this.top = newNode;
+                this.bottom = newNode;
+            } else {
+                newNode.next = this.top;
+                this.top = newNode;
+            }
+
+            this.length++;
+
+            return this;
+        }
+
+        isEmpty(): boolean {
+            if (this.length === 0) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
+
+    const myStack = new Stack();
+    myStack.push("google");
+    myStack.push("udemy");
+    myStack.push("discord");
+    console.log(myStack);
+    console.log(myStack.peek());
+
+    console.log(myStack.pop());
+    console.log(myStack.pop());
+    console.log(myStack.pop());
 }
-
-class Stack {
-  top: MyNode | null;
-  bottom: MyNode | null;
-  length: number;
-
-  constructor() {
-    this.top = null;
-    this.bottom = null;
-    this.length = 0;
-  }
-
-  peek(): MyNode | null {
-    return this.top;
-  }
-
-  pop(): MyNode | null {
-    if (!this.top) {
-      return null;
-    }
-
-    let result: MyNode | null = null;
-
-    result = this.top;
-    this.top = this.top.next;
-    result.next = null;
-    this.length--;
-
-    if (this.length === 0) {
-      this.bottom = null;
-    }
-
-    return result;
-  }
-
-  push(value: string): Stack {
-    const newNode: MyNode = new MyNode(value);
-
-    if (this.length === 0) {
-      this.top = newNode;
-      this.bottom = newNode;
-    } else {
-      newNode.next = this.top;
-      this.top = newNode;
-    }
-
-    this.length++;
-
-    return this;
-  }
-
-  isEmpty(): boolean {
-    if (this.length === 0) {
-      return true;
-    } else {
-      return false;
-    }
-  }
-}
-
-const myStack = new Stack();
-myStack.push("google");
-myStack.push("udemy");
-myStack.push("discord");
-console.log(myStack);
-console.log(myStack.peek());
-
-console.log(myStack.pop());
-console.log(myStack.pop());
-console.log(myStack.pop());
