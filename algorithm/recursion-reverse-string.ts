93301ff0b4841ca8f282a201a925969d4635cee1
evaou <speeder528@gmail.com>
2020-12-29 07:59:51 +0800

    add recursive and iterative reverse string

---
+++ algorithm/recursion-reverse-string.ts
@@ -0,0 +1,26 @@
+// tsc recursion-reverse-string.ts --downlevelIteration
+
+function reverseStringIterative(str: string): string {
+    let strArray: string[] = [...str];
+    let strLength: number = str.length;
+    let tmp: string;
+
+    for (let i = 0; i < strLength / 2; i++) {
+        tmp = strArray[i];
+        strArray[i] = strArray[strLength - 1 - i];
+        strArray[strLength - 1 - i] = tmp;
+    }
+
+    return strArray.join("");
+}
+
+function reverseStringRecursive(str: string): string {
+    if (!str.length) {
+        return "";
+    }
+
+    return reverseStringRecursive(str.substr(1)) + str[0];
+}
+
+console.log(reverseStringIterative("yoyo mastery"));
+console.log(reverseStringRecursive("yoyo mastery"));
