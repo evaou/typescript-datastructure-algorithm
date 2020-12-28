0a420a3916bb9467de5936a380189a07a08f0448
evaou <speeder528@gmail.com>
2020-12-28 13:37:02 +0800

    add namespace and update format

--- data-structure/queue-by-linked-list.ts
+++ data-structure/queue-by-linked-list.ts
@@ -1,69 +1,71 @@
-class MyNode {
-  value: string;
-  next: MyNode | null;
+namespace QueueByLinkedList {
+    class Node {
+        value: string;
+        next: Node | null;
 
-  constructor(value: string) {
-    this.value = value;
-    this.next = null;
-  }
+        constructor(value: string) {
+            this.value = value;
+            this.next = null;
+        }
+    }
+
+    class Queue {
+        first: Node | null;
+        last: Node | null;
+        length: number;
+
+        constructor() {
+            this.first = null;
+            this.last = null;
+            this.length = 0;
+        }
+
+        peek(): Node | null {
+            return this.first;
+        }
+
+        enqueue(value: string): Queue {
+            const newNode: Node = new Node(value);
+
+            if (this.length === 0) {
+                this.first = newNode;
+                this.last = newNode;
+            } else {
+                this.last.next = newNode;
+                this.last = newNode;
+            }
+
+            this.length++;
+
+            return this;
+        }
+
+        dequeue(): Node | null {
+            if (!this.first) {
+                return null;
+            }
+
+            if (this.first === this.last) {
+                this.last = null;
+            }
+
+            const resultNode: Node | null = this.first;
+
+            this.first = this.first.next;
+            resultNode.next = null;
+            this.length--;
+
+            return resultNode;
+        }
+    }
+
+    var myQueue = new Queue();
+    myQueue.enqueue("Joy");
+    myQueue.enqueue("Matt");
+    myQueue.enqueue("Pavel");
+    myQueue.enqueue("Samir");
+    console.log(myQueue);
+    console.log(myQueue.peek());
+    myQueue.dequeue();
+    console.log(myQueue.peek());
 }
-
-class Queue {
-  first: MyNode | null;
-  last: MyNode | null;
-  length: number;
-
-  constructor() {
-    this.first = null;
-    this.last = null;
-    this.length = 0;
-  }
-
-  peek(): MyNode | null {
-    return this.first;
-  }
-
-  enqueue(value: string): Queue {
-    const newNode: MyNode = new MyNode(value);
-
-    if (this.length === 0) {
-      this.first = newNode;
-      this.last = newNode;
-    } else {
-      this.last.next = newNode;
-      this.last = newNode;
-    }
-
-    this.length++;
-
-    return this;
-  }
-
-  dequeue(): MyNode | null {
-    if (!this.first) {
-      return null;
-    }
-
-    if (this.first === this.last) {
-      this.last = null;
-    }
-
-    const resultNode: MyNode | null = this.first;
-
-    this.first = this.first.next;
-    resultNode.next = null;
-    this.length--;
-
-    return resultNode;
-  }
-}
-
-var myQueue = new Queue();
-myQueue.enqueue("Joy");
-myQueue.enqueue("Matt");
-myQueue.enqueue("Pavel");
-myQueue.enqueue("Samir");
-console.log(myQueue);
-console.log(myQueue.peek());
-myQueue.dequeue();
-console.log(myQueue.peek());
