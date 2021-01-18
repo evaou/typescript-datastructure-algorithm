08e435ae776d4d12577cd17b88e4ea8b5388ef53
evaou <speeder528@gmail.com>
2021-01-18 20:05:10 +0800

    solve leetcode 42

---
+++ leetcode/42-trapping-rain-water.ts
@@ -0,0 +1,51 @@
+function getTrappedWater(area: number[][], height: number[]): number {
+    let sum = 0;
+
+    for (let pairP of area) {
+        const p1 = pairP[0];
+        const p2 = pairP[1];
+        const areaWidth = Math.abs(p1 - p2) - 1;
+        const areaHight = Math.min(height[p1], height[p2]);
+        let totalArea = areaWidth * areaHight;
+
+        for (
+            let index = Math.min(p1, p2) + 1;
+            index < Math.max(p1, p2);
+            index++
+        ) {
+            totalArea -= height[index];
+        }
+
+        sum += totalArea;
+    }
+
+    return sum;
+}
+
+function trap(height: number[]): number {
+    let waterArea: number[][] = [];
+    let leftP = 0;
+    let rightP = height.length - 1;
+
+    while (leftP < rightP) {
+        if (height[leftP] <= height[rightP]) {
+            for (let tmpP = leftP + 1; tmpP <= rightP; tmpP++) {
+                if (height[tmpP] >= height[leftP]) {
+                    waterArea.push([leftP, tmpP]);
+                    leftP = tmpP;
+                    break;
+                }
+            }
+        } else {
+            for (let tmpP = rightP - 1; tmpP >= leftP; tmpP--) {
+                if (height[tmpP] >= height[rightP]) {
+                    waterArea.push([rightP, tmpP]);
+                    rightP = tmpP;
+                    break;
+                }
+            }
+        }
+    }
+
+    return getTrappedWater(waterArea, height);
+}
