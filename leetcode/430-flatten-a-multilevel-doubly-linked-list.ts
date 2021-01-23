176db0082c8a82a6c1d010b4078d8d21eb91d293
evaou <speeder528@gmail.com>
2021-01-24 07:43:52 +0800

    add node class

--- leetcode/430-flatten-a-multilevel-doubly-linked-list.ts
+++ leetcode/430-flatten-a-multilevel-doubly-linked-list.ts
@@ -1,35 +1,48 @@
-function flatten(head: Node | null): Node | null {
-    if (
-        head === null ||
-        (head.next === null && head.child === null && head.prev === null)
-    ) {
-        return head;
+namespace leetcode430 {
+    class Node {
+        val: number;
+        next: Node | null;
+        prev: Node | null;
+        child: Node | null;
+
+        constructor(val?: number, next?: Node | null, child?: Node | null) {
+            this.val = val === undefined ? 0 : val;
+            this.next = next === undefined ? null : next;
+            this.prev = next === undefined ? null : next;
+            this.child = child === undefined ? null : child;
+        }
     }
 
-    let current = head;
-    let currentNext: Node | null;
-
-    while (current !== null) {
-        if (current.child !== null) {
-            currentNext = current.next;
-            current.next = current.child;
-            current.child.prev = current;
-
-            let childNextTail = current.child;
-            while (childNextTail.next !== null) {
-                childNextTail = childNextTail.next;
-            }
-
-            if (currentNext !== null) {
-                childNextTail.next = currentNext;
-                currentNext.prev = childNextTail;
-            }
-
-            current.child = null;
+    function flatten(head: Node | null): Node | null {
+        if (head === null) {
+            return head;
         }
 
-        current = current.next;
-    }
+        let current = head;
+        let currentNext: Node | null;
 
-    return head;
+        while (current !== null) {
+            if (current.child !== null) {
+                currentNext = current.next;
+                current.next = current.child;
+                current.child.prev = current;
+
+                let childNextTail = current.child;
+                while (childNextTail.next !== null) {
+                    childNextTail = childNextTail.next;
+                }
+
+                if (currentNext !== null) {
+                    childNextTail.next = currentNext;
+                    currentNext.prev = childNextTail;
+                }
+
+                current.child = null;
+            }
+
+            current = current.next;
+        }
+
+        return head;
+    }
 }
