7430c082f47d0cd977fbd4f06c2e210a85d707a6
evaou <speeder528@gmail.com>
2021-01-27 23:34:07 +0800

    add additional solution

--- leetcode/104-maximum-depth-of-binary-tree.ts
+++ leetcode/104-maximum-depth-of-binary-tree.ts
@@ -1,7 +1,21 @@
 function maxDepth(root: TreeNode | null): number {
-    if (!root) {
-        return 0;
-    }
+  if (!root) {
+    return 0;
+  }
 
-    return Math.max(maxDepth(root.left) + 1, maxDepth(root.right) + 1);
+  return Math.max(maxDepth(root.left) + 1, maxDepth(root.right) + 1);
+}
+
+function DFS(node: TreeNode | null, count: number): number {
+  if (node === null) {
+    return count;
+  }
+
+  count++;
+
+  return Math.max(DFS(node.left, count), DFS(node.right, count));
+}
+// time: O(N), space: O(N)
+function maxDepth2(root: TreeNode | null): number {
+  return DFS(root, 0);
 }
