ca8b5bfaf0f71eedb1dbada0e66ac3858b728eb9
evaou <speeder528@gmail.com>
2021-02-01 17:19:27 +0800

    add exercise for reverse operations

---
+++ exercise/linked-list-reverse-operations.ts
@@ -0,0 +1,72 @@
+/*
+Reverse Operations
+You are given a singly-linked list that contains N integers. A subpart of the list is a contiguous set of even elements, bordered either by either end of the list or an odd element. For example, if the list is [1, 2, 8, 9, 12, 16], the subparts of the list are [2, 8] and [12, 16].
+Then, for each subpart, the order of the elements is reversed. In the example, this would result in the new list, [1, 8, 2, 9, 16, 12].
+The goal of this question is: given a resulting list, determine the original order of the elements.
+Implementation detail:
+You must use the following definition for elements in the linked list:
+class Node {
+    int data;
+    Node next;
+}
+
+Signature
+Node reverse(Node head)
+
+Constraints
+1 <= N <= 1000, where N is the size of the list
+1 <= Li <= 10^9, where Li is the ith element of the list
+
+Example
+Input:
+N = 6
+list = [1, 2, 8, 9, 12, 16]
+Output:
+[1, 8, 2, 9, 16, 12]
+ */
+
+class Node {
+  data: number;
+  next: Node | null;
+
+  constructor(data: number) {
+    this.data = data;
+    this.next = null;
+  }
+}
+
+function reverse(head): void {
+  let preStart = null;
+  let evens: Node[] = [];
+  let current = head;
+
+  while (current !== null) {
+    if (current.data % 2 === 0) {
+      evens.push(current);
+    } else {
+      while (evens.length > 0) {
+        if (preStart) {
+          preStart.next = evens.pop();
+          preStart = preStart.next;
+        } else {
+          preStart = evens.pop();
+          head = preStart;
+        }
+      }
+      if (preStart) {
+        preStart.next = current;
+      }
+    }
+
+    preStart = current;
+    current = current.next;
+  }
+
+  while (evens.length > 0) {
+    preStart.next = evens.pop();
+    preStart = preStart.next;
+  }
+  preStart.next = null;
+
+  return head;
+}
