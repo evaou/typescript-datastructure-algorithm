5897fd0fc239134f94c105c7d6c4efea2715b2fc
evaou <speeder528@gmail.com>
2021-02-02 20:48:34 +0800

    add bfs for graph adjacency list

---
+++ algorithm/breadth-first-search-adjacency-list.ts
@@ -0,0 +1,34 @@
+let adjList = [
+  [1, 3],
+  [0],
+  [3, 8],
+  [0, 2, 4, 5],
+  [3, 6],
+  [3],
+  [4, 7],
+  [6],
+  [2],
+];
+
+function graphBFS(graph: number[][]): number[] {
+  let result: number[] = [];
+  let queue: number[] = [0];
+  let seen = new Set();
+
+  while (queue.length > 0) {
+    let current = queue.shift();
+    result.push(current);
+    seen.add(current);
+
+    for (let adjNode of graph[current]) {
+      if (seen.has(adjNode)) {
+        continue;
+      }
+      queue.push(adjNode);
+    }
+  }
+
+  return result;
+}
+
+console.log(graphBFS(adjList));
