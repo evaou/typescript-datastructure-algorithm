676ccde60855e015e19a01a8468e96a4c403d485
evaou <speeder528@gmail.com>
2021-02-22 21:18:42 +0800

    update bubble sort

--- algorithm/bubble-sort.ts
+++ algorithm/bubble-sort.ts
@@ -1,22 +1,29 @@
+// time: O(N^2), space: O(1)
 function bubbleSort(arr: number[]): number[] {
-    for (let endIdx = arr.length - 1; endIdx > 0; endIdx--) {
-        for (let startIdx = 0; startIdx < endIdx; startIdx++) {
-            if (arr[startIdx] > arr[endIdx]) {
-                swap(arr, startIdx, endIdx);
-            }
-        }
+  let isSwapped: boolean;
+
+  for (let endIdx = arr.length - 1; endIdx > 0; endIdx--) {
+    isSwapped = false;
+
+    for (let startIdx = 0; startIdx < endIdx; startIdx++) {
+      if (arr[startIdx] > arr[startIdx + 1]) {
+        swap(arr, startIdx, startIdx + 1);
+        isSwapped = true;
+      }
     }
 
-    return arr;
+    if (!isSwapped) {
+      break;
+    }
+  }
+
+  return arr;
 }
 
 function swap(arr: number[], idx1: number, idx2: number): void {
-    let tmpVal: number;
+  let tmpVal: number;
 
-    tmpVal = arr[idx1];
-    arr[idx1] = arr[idx2];
-    arr[idx2] = tmpVal;
+  tmpVal = arr[idx1];
+  arr[idx1] = arr[idx2];
+  arr[idx2] = tmpVal;
 }
-
-const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
-console.log("sorted number: " + bubbleSort(numbers));
