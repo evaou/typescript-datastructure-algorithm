3d31e363f612573f9ec4fd0356314a4cf33d2836
evaou <speeder528@gmail.com>
2021-02-22 21:56:00 +0800

    add time and space complexity for selection sort

--- algorithm/selection-sort.ts
+++ algorithm/selection-sort.ts
@@ -1,31 +1,32 @@
-namespace SelectionSort {
-    function selectionSort(arr: number[]): number[] {
-        let minValIdx: number;
+// worst time: O(N^2), best time: O(N^2)
+// space: O(1)
+function selectionSort(arr: number[]): number[] {
+  let minValIdx: number;
 
-        for (let targetIdx = 0; targetIdx < arr.length - 1; targetIdx++) {
-            minValIdx = targetIdx;
-            for (let currentIdx = targetIdx + 1; currentIdx < arr.length; currentIdx++) {
-                if (arr[currentIdx] < arr[minValIdx]) {
-                    minValIdx = currentIdx;
-                }
-            }
-
-            if (minValIdx != targetIdx) {
-                swap(arr, targetIdx, minValIdx);
-            }
-        }
-
-        return arr;
+  for (let targetIdx = 0; targetIdx < arr.length - 1; targetIdx++) {
+    minValIdx = targetIdx;
+    for (
+      let currentIdx = targetIdx + 1;
+      currentIdx < arr.length;
+      currentIdx++
+    ) {
+      if (arr[currentIdx] < arr[minValIdx]) {
+        minValIdx = currentIdx;
+      }
     }
 
-    function swap(arr: number[], idx1: number, idx2: number): void {
-        let tmpVal: number;
-    
-        tmpVal = arr[idx1];
-        arr[idx1] = arr[idx2];
-        arr[idx2] = tmpVal;
+    if (minValIdx != targetIdx) {
+      swap(arr, targetIdx, minValIdx);
     }
+  }
 
-    const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
-    console.log("sorted number: " + selectionSort(numbers));
+  return arr;
+}
+
+function swap(arr: number[], idx1: number, idx2: number): void {
+  let tmpVal: number;
+
+  tmpVal = arr[idx1];
+  arr[idx1] = arr[idx2];
+  arr[idx2] = tmpVal;
 }
