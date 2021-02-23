8932a35cea6ad00234c88db8743c887f7e4117c2
evaou <speeder528@gmail.com>
2021-02-23 08:04:27 +0800

    update queue implementation

--- data-structure/queue-by-stack.ts
+++ data-structure/queue-by-stack.ts
@@ -1,58 +1,56 @@
 namespace QueueByStack {
-    class Queue {
-        first: number[];
-        last: number[];
+  class Queue {
+    first: number[];
+    last: number[];
 
-        constructor() {
-            this.first = [];
-            this.last = [];
-        }
-
-        enqueue(x: number): void {
-            let firstLength: number = this.first.length;
-            for (let i = 0; i < firstLength; i++) {
-                this.last.push(this.first.pop());
-            }
-            this.last.push(x);
-        }
-
-        dequeue(): number {
-            let lastLength: number = this.last.length;
-            for (let i = 0; i < lastLength; i++) {
-                this.first.push(this.last.pop());
-            }
-
-            return this.first.pop();
-        }
-
-        peek(): number {
-            if (this.last.length > 0) {
-                return this.last[0];
-            }
-
-            return this.first[this.first.length - 1];
-        }
-
-        isEmpty(): boolean {
-            if (this.first.length || this.last.length) {
-                return false;
-            }
-
-            return true;
-        }
+    constructor() {
+      this.first = [];
+      this.last = [];
     }
 
-    let myQueue: Queue = new Queue();
-    myQueue.enqueue(1);
-    myQueue.enqueue(2);
-    myQueue.enqueue(3);
-    myQueue.enqueue(4);
+    enqueue(x: number): void {
+      this.last.push(x);
+    }
 
-    console.log(myQueue.dequeue());
-    myQueue.enqueue(5);
+    dequeue(): number {
+      if (this.first.length === 0) {
+        let lastLength: number = this.last.length;
+        for (let i = 0; i < lastLength; i++) {
+          this.first.push(this.last.pop());
+        }
+      }
 
-    console.log(myQueue.dequeue());
-    console.log(myQueue.dequeue());
-    console.log(myQueue.dequeue());
-    console.log(myQueue.dequeue());
+      return this.first.pop();
+    }
+
+    peek(): number {
+      if (this.first.length > 0) {
+        return this.first[this.first.length - 1];
+      } else {
+        return this.last[0];
+      }
+    }
+
+    isEmpty(): boolean {
+      if (this.first.length || this.last.length) {
+        return false;
+      }
+
+      return true;
+    }
+  }
+
+  let myQueue: Queue = new Queue();
+  myQueue.enqueue(1);
+  myQueue.enqueue(2);
+  myQueue.enqueue(3);
+  myQueue.enqueue(4);
+
+  console.log(myQueue.dequeue());
+  myQueue.enqueue(5);
+
+  console.log(myQueue.dequeue());
+  console.log(myQueue.dequeue());
+  console.log(myQueue.dequeue());
+  console.log(myQueue.dequeue());
 }
