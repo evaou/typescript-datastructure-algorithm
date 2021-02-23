db07e3a0afaf711fcbc28a78100668c8a4b91b41
evaou <speeder528@gmail.com>
2021-02-23 12:20:00 +0800

    update recursive fibonacci

--- algorithm/recursion-fibonacci.ts
+++ algorithm/recursion-fibonacci.ts
@@ -1,27 +1,27 @@
 function fibonacciIterative(num: number): number {
-    if (num <= 1) {
-        return num;
-    }
+  if (num <= 1) {
+    return num;
+  }
 
-    let sum: number = 1;
-    let first: number = 0;
-    let second: number = 1;
+  let sum: number;
+  let first: number = 0;
+  let second: number = 1;
 
-    for (let i = 2; i <= num; i++) {
-        sum = first + second;
-        first = second;
-        second = sum;
-    }
+  for (let i = 2; i <= num; i++) {
+    sum = first + second;
+    first = second;
+    second = sum;
+  }
 
-    return sum;
+  return sum;
 }
 
 function fibonacciRecursive(num): number {
-    if (num <= 1) {
-        return num;
-    }
+  if (num <= 1) {
+    return num;
+  }
 
-    return fibonacciRecursive(num - 1) + fibonacciRecursive(num - 2);
+  return fibonacciRecursive(num - 1) + fibonacciRecursive(num - 2);
 }
 
 console.log(fibonacciIterative(6));
